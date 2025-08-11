e9a2d8eb3939153c3d075c81afdba6654f2e520c
TheBoolean-Boy <saswatrath02@gmail.com>
2025-08-11 23:39:45 +0530

    Comments components developed

--- src/Components/PlayVideo/PlayVideo.jsx
+++ src/Components/PlayVideo/PlayVideo.jsx
@@ -12,41 +12,45 @@ import moment from 'moment'
 import { likeCounter } from '../../likeCounter'
 import ReactLinkify from 'react-linkify'
 
-function PlayVideo({videoId}) {
+function PlayVideo({ videoId }) {
   const [videodata, setVideodata] = useState();
   const [channelData, setChannelData] = useState();
-  const [commentData, setCommentData] = useState();
+  const [commentData, setCommentData] = useState([]);
 
-  const fetchVideoData = async () =>{
+  const fetchVideoData = async () => {
     const api_url = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=${videoId}&key=${import.meta.env.VITE_YOUTUBE_API_KEY}`
     await fetch(api_url).then(response => response.json()).then(data => setVideodata(data.items[0]))
 
   }
 
-  const fetchChannelData = async () =>{
+  const fetchChannelData = async () => {
     const api_url = `https://youtube.googleapis.com/youtube/v3/channels?part=snippet%2CcontentDetails%2Cstatistics&id=${videodata?.snippet?.channelId}&key=${import.meta.env.VITE_YOUTUBE_API_KEY}`
-    await fetch(api_url).then( response => response.json()).then( data => setChannelData(data.items[0]))
+    await fetch(api_url).then(response => response.json()).then(data => setChannelData(data.items[0]))
 
   }
 
   const fetchComment = async () => {
-    const comment_url =  `https://youtube.googleapis.com/youtube/v3/commentThreads?part=snippet%2Creplies&videoId=_${videoId}VB39Jo8mAQ&key=${import.meta.env.VITE_YOUTUBE_API_KEY}`
+    const comment_url = `https://youtube.googleapis.com/youtube/v3/commentThreads?part=snippet%2Creplies&maxResults=100&videoId=${videoId}&key=${import.meta.env.VITE_YOUTUBE_API_KEY}`
     await fetch(comment_url).then(response => response.json()).then(data => setCommentData(data.items))
   }
 
-  useEffect ( () => {
+  useEffect(() => {
     fetchVideoData();
-  },[videoId])
-  
-  useEffect ( () => {
+  }, [videoId])
+
+  useEffect(() => {
     fetchChannelData();
   }, [videodata])
+
+  useEffect( () => {
+    fetchComment();
+  }, [videoId])
 
   return (
     <div className='play-video'>
 
       {/* <video src={video1} controls autoPlay muted></video> */}
-      <iframe src={`https://www.youtube.com/embed/${videoId}?autoplay=1&mute=1`}  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+      <iframe src={`https://www.youtube.com/embed/${videoId}?autoplay=1&mute=1`} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
       <h3>{videodata?.snippet?.title}</h3>
 
       <div className="play-video-info">
@@ -73,19 +77,26 @@ function PlayVideo({videoId}) {
         <p> <ReactLinkify >{videodata?.snippet?.description}</ReactLinkify></p>
         <hr />
         <h4>{videodata?.statistics?.commentCount} Comments</h4>
-        <div className="comment">
-          <img src={user_profile} alt="" />
-          <div>
-            <h3>Jack Nicholson <span>1 day ago</span></h3>
-            <p>A global computer network providing a variety of information and cc of interconnected networks using standardized communication protocols.</p>
-            <div className="comment-action">
-              <img src={like} alt="" />
-              <span>244</span>
-              <img src={dislike} alt="" />
-            </div>
-          </div>
-        </div>
-        
+        {
+          commentData?.map((item, index) => {
+            return (
+              <div key={index} className="comment">
+                <img src={item?.snippet?.topLevelComment?.snippet?.authorProfileImageUrl} alt="" />
+                <div>
+                  <h3>{item?.snippet?.topLevelComment?.snippet?.authorDisplayName}<span>{moment(item?.snippet?.topLevelComment?.snippet?.updatedAt).fromNow()}</span></h3>
+                  <p>{item?.snippet?.topLevelComment?.snippet?.textDisplay}</p>
+                  <div className="comment-action">
+                    <img src={like} alt="" />
+                    <span>{likeCounter(item?.snippet?.topLevelComment?.snippet?.likeCount)}</span>
+                    <img src={dislike} alt="" />
+                  </div>
+                </div>
+              </div>
+            )
+          })
+        }
+
+
       </div>
 
 
