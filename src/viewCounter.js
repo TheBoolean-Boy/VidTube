40be5eb9e33ea3aaeffbde4fdaa7d65598f004a1
Saswat Rath <94009506+TheBoolean-Boy@users.noreply.github.com>
2025-10-03 15:09:35 +0530

    refactor: modified old code with better modern practices

--- src/viewCounter.js
+++ src/viewCounter.js
@@ -1,12 +1,9 @@
-
-
 export const viewCounter = (views) => {
-  if(views >= 1000000){
-    return Math.floor(views/1000000)+"M";
-  }
-  else if(views >=1000){
-    return Math.floor(views/1000)+"K";
-  }else{
+  if (views >= 1000000) {
+    return `${Math.floor(views / 1000000)}M`;
+  } else if (views >= 1000) {
+    return `${Math.floor(views / 1000)}K`;
+  } else {
     return views;
   }
-}
+};
